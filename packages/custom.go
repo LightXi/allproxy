771b59c71bf03422b6ded47e04a9297372d9a3a2
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-10-04 09:09:52 +0800

    fix custom

--- packages/custom.go
+++ packages/custom.go
@@ -12,10 +12,11 @@ var CustomReplacer = map[string]string{
 }
 
 func RegisterCustom(app *fiber.App) {
-	for _, v := range CustomReplacer {
-		app.All(fmt.Sprintf("/%s/*", v), func(c *fiber.Ctx) error {
-			source := strings.TrimPrefix(c.Path(), fmt.Sprintf("/%s", v))
+	for k, v := range CustomReplacer {
+		app.All(fmt.Sprintf("/%s/*", k), func(c *fiber.Ctx) error {
+			source := strings.TrimPrefix(c.Path(), fmt.Sprintf("/%s", k))
 			uri := fmt.Sprintf("%s%s", v, source)
+			fmt.Println(uri)
 			resp, err := utils.Get(uri, nil)
 			if err != nil {
 				return Catch(c, err)
